45ea392527ce2f8d95cc03b74911f777e6af317a
MisterSpacebar <yibinwei@live.com>
2026-04-09 15:40:46 -0400

    update for security

--- src/firebase.js
+++ src/firebase.js
@@ -3,13 +3,13 @@ import { getAnalytics } from "firebase/analytics";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAnejaCo0rikhwbaqwuOnaVi0z1M3G1rSg",
-  authDomain: "react-plots.firebaseapp.com",
-  projectId: "react-plots",
-  storageBucket: "react-plots.firebasestorage.app",
-  messagingSenderId: "543057660410",
-  appId: "1:543057660410:web:9aac79423ebe4ecfbb3c43",
-  measurementId: "G-ZGE46ZJNP3",
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID,
 };
 
 const app = initializeApp(firebaseConfig);
